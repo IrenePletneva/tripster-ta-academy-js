d24157385ce23f757eca27f1695ffc07fb9c8b71
Irene <irenepletneva1997@gmail.com>
2024-12-09 19:57:20 +0300

    homework-2

---
+++ homework-2/index.js
@@ -0,0 +1,48 @@
+// Task 1
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+// Task 2
+function isEmpty(obj) {
+    for (let key in obj){
+        return false;
+    }
+    return true;
+}
+
+// Task 3
+// Объект, объявленный через const, может быть изменён.
+const user = {
+    name: "John"
+  };
+  
+  // это будет работать? - да будет
+  user.name = "Pete";
+
+// Task 4
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+let sum = 0;
+for(let key in salaries){
+    sum += salaries[key];
+}
+console.log(sum);
+
+// Task 5
+function multiplyNumeric(obj) {
+    for (let key in obj){
+        if (typeofobj[key] == 'number'){
+            obj[key] *= 2;
+        }
+    }
+}
+
+// Task 6
+
+
